e963faa01f34de834a8b4062f1aa880dd1771f5c
Altecrow <romainsteciuk@gmail.com>
2025-01-20 18:31:14 +0100

    Create backtest_logic.py

---
+++ backtest_logic.py
@@ -0,0 +1,57 @@
+in_pos = None
+stop_loss = 0.005
+rr = 4
+strat_logs = []
+
+for index, row in data.iterrows():
+    if in_pos == 'long':
+        if row.ohlc4 <= actual_stop:
+            # Stop loss atteint
+            exit_price = actual_stop
+            exit_time = index
+            pnl = exit_price - entry_price
+            strat_logs.append([exit_price, entry_price, in_pos, entry_time, exit_time, pnl])
+            in_pos = None
+        elif row.ohlc4 >= take_profit:
+            # Take profit atteint
+            exit_price = take_profit
+            exit_time = index
+            pnl = exit_price - entry_price
+            strat_logs.append([exit_price, entry_price, in_pos, entry_time, exit_time, pnl])
+            in_pos = None
+    elif in_pos == 'short':
+        if row.ohlc4 >= actual_stop:
+            # Stop loss atteint
+            exit_price = actual_stop
+            exit_time = index
+            pnl = entry_price - exit_price
+            strat_logs.append([exit_price, entry_price, in_pos, entry_time, exit_time, pnl])
+            in_pos = None
+        elif row.ohlc4 <= take_profit:
+            # Take profit atteint
+            exit_price = take_profit
+            exit_time = index
+            pnl = entry_price - exit_price
+            strat_logs.append([exit_price, entry_price, in_pos, entry_time, exit_time, pnl])
+            in_pos = None
+    else:
+        # Pas en position
+        if row.ohlc4 > row.fvg_down and row.ohlc4 < row.fvg_up:
+            if row.ohlc4 > row.buy_breaker_block_low and row.ohlc4 < row.buy_breaker_block_high:
+                # Entrée en position longue
+                in_pos = 'long'
+                entry_price = row.ohlc4
+                actual_stop = entry_price * (1 - stop_loss)
+                stop_distance = entry_price - actual_stop
+                take_profit = entry_price + stop_distance * rr
+                entry_time = index
+            elif row.ohlc4 > row.sell_breaker_block_low and row.ohlc4 < row.sell_breaker_block_high:
+                # Entrée en position courte
+                in_pos = 'short'
+                entry_price = row.ohlc4
+                actual_stop = entry_price * (1 + stop_loss)
+                stop_distance = actual_stop - entry_price
+                take_profit = entry_price - stop_distance * rr
+                entry_time = index
+
+strat_logs = pd.DataFrame(strat_logs, columns=['exit_price', 'entry_price', 'type', 'entry_time', 'exit_time', 'pnl'])
